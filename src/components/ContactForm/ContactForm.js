74a04cb06b138a7b001dd5732dce734b5c0a6d9b
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-11-23 23:58:24 +0200

    clone hw-2

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import s from './ContactForm.module.css';
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputOnChange = event => {
+    const { name, value } = event.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className={s.formGroup}>
+          <label>
+            Name{' '}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+              onChange={this.handleInputOnChange}
+            />
+          </label>
+
+          <label>
+            Number{' '}
+            <input
+              type="tel"
+              name="number"
+              value={number}
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+              required
+              onChange={this.handleInputOnChange}
+            />
+          </label>
+        </div>
+
+        <button type="submit" className={s.button}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
